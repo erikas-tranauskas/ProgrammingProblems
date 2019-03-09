dd28c6d34e4c26baa75adfce31a485a81be18b44
Erikas <e50erikas@gmail.com>
2019-03-10 00:19:08 +0200

    FizzBuzz

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -5,7 +5,7 @@
     But for multiples of three print “Fizz” instead of the number
     and for the multiples of five print “Buzz”.
     For numbers which are multiples of both three and five print “FizzBuzz”.
- **/
+ */
 
 namespace App;
 
