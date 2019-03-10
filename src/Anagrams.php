f8366407b5cc759daa3ca186c50dbf21c5068941
Erikas <e50erikas@gmail.com>
2019-03-10 18:43:40 +0200

    Anagrams

---
+++ src/Anagrams.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+    Compare two string if they are anagrams of each other.
+    Anagram is string which can be made from symbols of another string using same characters in the same quantity.
+    Do not take spaces and punctuation into consideration.
+ */
+
+namespace App;
+
+class Anagrams
+{
+    public function index($string1, $string2)
+    {
+        $splitString1 = $this->splitString($string1);
+        $splitString2 = $this->splitString($string2);
+
+        if (count($splitString1) !== count($splitString2)) {
+            return false;
+        }
+
+        foreach ($splitString1 as $key => $single) {
+            if ($splitString1[$key] !== $splitString2[$key]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function splitString($string)
+    {
+        $characters = array();
+        $cleaned = preg_replace("/[^A-Za-z0-9]/", "", $string);
+
+        foreach (str_split(strtolower($cleaned)) as $char) {
+            $characters[$char] = $characters[$char] + 1;
+        }
+
+        return $characters;
+    }
+}
---
+++ tests/AnagramsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\FizzBuzzTest;
+
+use PHPUnit\Framework\TestCase;
+use App\Anagrams;
+
+class AnagramsTest extends TestCase
+{
+    /**
+     * @dataProvider provider
+     * @param string $inputString1
+     * @param string $inputString2
+     * @param array $expectedResults
+     *
+     */
+    public function testNormalInputSet($inputString1, $inputString2, $expectedResults)
+    {
+        $anagrams = new Anagrams();
+        $testResults = $anagrams->index($inputString1, $inputString2);
+
+        $this->assertEquals($expectedResults, $testResults);
+    }
+
+    public function provider()
+    {
+        return array(
+            array("Rail Safety!", "Fairy Tales", true),
+            array("Hello", "Hello", true),
+            array("Rail Safety", "Fairy Tales", true),
+            array("Rail Safety!", "Fairy Tales", true),
+            array("Rail Safety!", "Fairy Tales2", false)
+        );
+    }
+}
